3ad1cb2d858aced4d575cf01c3ee84e641950bf3
Jeudi Prando <jeudi.prando@eletromidia.com.br>
2023-09-14 13:26:18 -0300

    refact(log):♻️  ajuste nas mensagens do gracefulshutdown

--- src/repository/gracefulShutdown.ts
+++ src/repository/gracefulShutdown.ts
@@ -3,9 +3,9 @@ import type { MongoClient } from "mongodb";
 import desconectar from "./desconectar";
 
 export default (conexao: MongoClient) => {
-    for (let evento of ['SIGINT', 'SIGTERM']) {
+    for (const evento of ['SIGINT', 'SIGTERM']) {
         process.on(evento, async () => {
-            console.info(`\n#INFO:${evento.toUpperCase()} encerrando aplicacao...`);
+            console.info(`\n#INFO:APP [${evento.toUpperCase()}] encerrando aplicacao...`);
             await desconectar(conexao);
             console.info("#INFO:APP aplicacao encerrada!");
             process.exit(0);
